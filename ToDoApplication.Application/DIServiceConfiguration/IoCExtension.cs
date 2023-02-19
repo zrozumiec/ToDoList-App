6d5ad44d7b1e128777cd37d30d67f92ab20212c4
Kamil Ciupak <kamil.ciupak@outlook.com>
2023-02-19 11:04:41 +0100

    Add extension class to register methods for Di

---
+++ ToDoApplication.Application/DIServiceConfiguration/IoCExtension.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ToDoApplication.Application.Interfaces;
+using ToDoApplication.Application.Mapper;
+using ToDoApplication.Application.Services;
+
+namespace ToDoApplication.Application.DIServiceConfiguration
+{
+    /// <summary>
+    /// Static class contains extensions method to configuration DI services.
+    /// </summary>
+    public static class IoCExtension
+    {
+        /// <summary>
+        /// Inject application services to DI service.
+        /// </summary>
+        /// <param name="services">Extension IServiceCollection.</param>
+        public static void RegisterServices(this IServiceCollection services)
+        {
+            services.AddTransient<ITaskCategoryService, TaskCategoryService>();
+            services.AddTransient<ITaskNotesService, TaskNotesService>();
+            services.AddTransient<ITaskPriorityService, TaskPriorityService>();
+            services.AddTransient<ITaskStatusesService, TaskStatusesService>();
+            services.AddTransient<IToDoTaskService, ToDoTaskService>();
+            services.AddTransient<IToDoListService, ToDoListService>();
+        }
+
+        /// <summary>
+        /// Inject AutoMapper profiles to DI service.
+        /// </summary>
+        /// <param name="services">Extension IServiceCollection.</param>
+        public static void RegisterMappers(this IServiceCollection services)
+        {
+            services.AddAutoMapper(typeof(TaskCategoryMappingProfile));
+            services.AddAutoMapper(typeof(TaskNotesMappingProfile));
+            services.AddAutoMapper(typeof(TaskPriorityMappingProfile));
+            services.AddAutoMapper(typeof(TaskStatusesMappingProfile));
+            services.AddAutoMapper(typeof(ToDoTaskMappingProfile));
+            services.AddAutoMapper(typeof(ToDoListMappingProfile));
+        }
+    }
+}
---
+++ ToDoApplication.Infrastructure/DIServiceConfiguration/IoCExtension.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ToDoApplication.Domain.Interfaces;
+using ToDoApplication.Infrastructure.Repositories;
+
+namespace ToDoApplication.Infrastructure.DIServiceConfiguration
+{
+    /// <summary>
+    /// Static class contains extensions method to configuration DI services.
+    /// </summary>
+    public static class IoCExtension
+    {
+        /// <summary>
+        /// Inject application repositories to DI service.
+        /// </summary>
+        /// <param name="services">Extension IServiceCollection.</param>
+        public static void RegisterRepository(this IServiceCollection services)
+        {
+            services.AddTransient<ITaskCategoryRepository, TaskCategoryRepository>();
+            services.AddTransient<ITaskNotesRepository, TaskNotesRepository>();
+            services.AddTransient<ITaskPriorityRepository, TaskPriorityRepository>();
+            services.AddTransient<ITaskStatusesRepository, TaskStatusesRepository>();
+            services.AddTransient<IToDoListRepository, ToDoListRepository>();
+            services.AddTransient<IToDoTaskRepository, ToDoTaskRepository>();
+        }
+    }
+}
