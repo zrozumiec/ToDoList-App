1f60fa4454460ae71195d04179fc614d21cf5c9d
Kamil Ciupak <kamil.ciupak@outlook.com>
2023-02-24 20:10:59 +0100

    Change 'Description' property to 'Name'

--- ToDoApplication.Application/DTOs/TaskNotesDto.cs
+++ ToDoApplication.Application/DTOs/TaskNotesDto.cs
@@ -8,11 +8,6 @@ namespace ToDoApplication.Application.DTOs
     public class TaskNotesDto : BaseDto
     {
         /// <summary>
-        /// Gets or sets task note description.
-        /// </summary>
-        public string Description { get; set; } = string.Empty;
-
-        /// <summary>
         /// Gets or sets associated task Id.
         /// </summary>
         public int TaskId { get; set; }
